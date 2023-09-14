1997fbab2bf6abd0f0f6cd79c5c0b5f99a407db0
Jean <acalto2@gmail.com>
2023-09-14 19:23:33 +0200

    137. Backend and understand api section part 18

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +13,16 @@ use App\Http\Controllers\Api\UserController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::group(['namespace'=>'Api'], function(){
 
-Route::post('/login', [UserController::class, 'login']);
+    //Route::post('/login', [UserController::class, 'login']);
+    Route::post('/login', 'UserController@login');
+    Route::group(['middleware'=>'auth:sanctum'], function(){
+
+        Route::any('/courseList', [CourseController::class, 'courseList']);
+    });
+});
+
+
+
+
