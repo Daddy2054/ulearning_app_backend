ab62d77dfd38b94fbf451ed15bef3b69aff6818e
Jean <acalto2@gmail.com>
2023-09-17 22:05:22 +0200

    168. Update lesson model

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -12,9 +12,10 @@ class Lesson extends Model
     use DefaultDatetimeFormat;
 
     protected $casts = [
-        'video'=>'json'
+        'video' => 'json'
     ];
-    public function setVideoAttribute($value){
+    public function setVideoAttribute($value)
+    {
         //the below method json_encode converts the object to json from array
         /*
             'a'=>'val1',
@@ -28,10 +29,18 @@ class Lesson extends Model
 
         $this->attributes['video'] = json_encode(array_values($value));
     }
-    public function getVideoAttribute($value){
-
-
-        $this->attributes['video'] = array_values(json_decode($value, true)?:[]);
+    public function getVideoAttribute($value)
+    {
+        //dump($value);
+        $resVideo = json_decode($value, true) ?: [];
+        //dump($resVideo);
+        if (!empty($resVideo)) {
+            foreach ($resVideo as $k => $v) {
+                $resVideo[$k]["url"] = $v["url"];
+                $resVideo[$k]["thumbnail"] = $v["thumbnail"];
+            }
+        }
+   return $resVideo;
     }
 }
 
